79f3e432dbd11bdbe52cc4d2bf130fc9d2d3532b
unknown <alejandro.becerra@correounivalle.edu.co>
2020-09-10 15:14:42 -0500

    se hizo la conexion a BD firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,12 +3,23 @@ export const environment = {
 };
 
 export const firebaseConfig = {
-  apiKey: '<your-api-key>',
-  authDomain: '<your-project-authdomain>',
-  databaseURL: '<your-database-URL>',
-  projectId: '<your-project-id>',
-  storageBucket: '<your-storage-bucket>',
-  messagingSenderId: '<your-messaging-sender-id>',
-  appId: "<your-app-id>",
- 
+  apiKey: "AIzaSyDUOM5G7ONFPkeNYyL7B7tk3HLeCMY2jlQ",
+  authDomain: "bd-parcial-ee12c.firebaseapp.com",
+  databaseURL: "https://bd-parcial-ee12c.firebaseio.com",
+  projectId: "bd-parcial-ee12c",
+  storageBucket: "bd-parcial-ee12c.appspot.com",
+  messagingSenderId: "774704158485",
+  appId: "1:774704158485:web:a1677c6789e7c288943b1c",
+  measurementId: "G-YRJJTZZYLJ"
 };
+
+// export const firebaseConfig = {
+//   apiKey: '<your-api-key>',
+//   authDomain: '<your-project-authdomain>',
+//   databaseURL: '<your-database-URL>',
+//   projectId: '<your-project-id>',
+//   storageBucket: '<your-storage-bucket>',
+//   messagingSenderId: '<your-messaging-sender-id>',
+//   appId: "<your-app-id>",
+ 
+// };
